d7dba14a16eb0f5e2d2578cb87796685043aff8d
oguzhan17 <oguzhancevher61@gmail.com>
2020-09-23 17:28:01 +0300

    CardView

--- lib/main.dart
+++ lib/main.dart
@@ -16,38 +16,77 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Yemek"),
         ),
-        body: GetData(),
+        body: CreateCard(),
       ),
     );
   }
 }
 
-class GetData extends StatelessWidget {
+class CreateCard extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return CreateCardState();
+  }
+}
+
+class CreateCardState extends State{
   @override
   Widget build(BuildContext context) {
-    CollectionReference users = FirebaseFirestore.instance.collection('food');
-
     return StreamBuilder<QuerySnapshot>(
-      stream: users.snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text('Something went wrong');
+      stream: Firestore.instance.collection("food").snapshots(),
+      builder: (context,snapshot){
+        if(!snapshot.hasData){
+          return LinearProgressIndicator();
+        }else{
+          return buildBody(context, snapshot.data.documents);
         }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
-        }
-
-        return new ListView(
-          // ignore: deprecated_member_use
-          children: snapshot.data.documents.map((DocumentSnapshot document) {
-            return new ListTile(
-              title: new Text(document.data()['foodname']),
-              subtitle: new Text(document.data()['foodrating']),
-            );
-          }).toList(),
-        );
       },
+    );
+
+
+  }
+  Widget buildBody(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: EdgeInsets.only(top: 20.0),
+      children: snapshot.map<Widget> ((data) => buildListItem(context,data)).toList(),
+    );
+  }
+
+  buildListItem(BuildContext context, DocumentSnapshot data) {
+    final row = GetCard.fromSnapshot(data);
+    return Padding(
+      key: ValueKey(row.name),
+      padding: EdgeInsets.symmetric(horizontal: 16.0,vertical: 8.0),
+        child: Container(
+          decoration: BoxDecoration(),
+        child: Column(children: [
+               Container(
+                  constraints: BoxConstraints.tightForFinite(
+                      width: 400
+                  ),
+                  height: 200.0,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                          image: NetworkImage(row.photo),
+                          fit: BoxFit.fill))),
+          SizedBox(height: 7.0),
+        ]))
     );
   }
 }
+
+class GetCard {
+  String name;
+  String photo;
+  DocumentReference reference;
+
+  GetCard.fromMap(Map<String,dynamic> map, {this.reference})
+      :assert(map["name"]!=null), assert(map["photo"]!=null),
+        name = map["name"], photo = map["photo"];
+
+  GetCard.fromSnapshot(DocumentSnapshot snapshot)
+      :this.fromMap(snapshot.data(), reference:snapshot.reference);
+}
+
