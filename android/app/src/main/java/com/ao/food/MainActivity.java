c6ec4e82bc7fcb7a64778583167a2a5d3ae12bd7
oguzhan17 <oguzhancevher61@gmail.com>
2020-09-23 15:49:45 +0300

    Initial commit

---
+++ android/app/src/main/java/com/ao/food/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ao.food;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Yemek"),
+        ),
+        body: UserInformation(),
+      ),
+    );
+  }
+}
+
+class UserInformation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('food');
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: users.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return new ListView(
+          // ignore: deprecated_member_use
+          children: snapshot.data.documents.map((DocumentSnapshot document) {
+            return new ListTile(
+              title: new Text(document.data()['foodname']),
+              subtitle: new Text(document.data()['foodrating']),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
