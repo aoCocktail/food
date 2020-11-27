167f30472c9ddc9079e352268513327c44e93900
oguzhan17 <oguzhancevher61@gmail.com>
2020-11-27 15:04:26 +0300

    Detail Page

--- lib/food_detail_page.dart
+++ lib/food_detail_page.dart
@@ -13,11 +13,11 @@ class FoodDetail extends StatelessWidget {
 
   FoodDetail(
       {Key key,
-      @required this.passData,
-      @required this.passDataName,
-      @required this.passColor1,
-      @required this.passColor2,
-      @required this.passColor3})
+        @required this.passData,
+        @required this.passDataName,
+        @required this.passColor1,
+        @required this.passColor2,
+        @required this.passColor3})
       : super(key: key);
 
   @override
@@ -55,11 +55,11 @@ class CreateCards extends StatefulWidget {
   int passColor1, passColor2, passColor3;
   CreateCards(
       {Key key,
-      @required this.passData,
-      @required this.passDataName,
-      @required this.passColor1,
-      @required this.passColor2,
-      @required this.passColor3})
+        @required this.passData,
+        @required this.passDataName,
+        @required this.passColor1,
+        @required this.passColor2,
+        @required this.passColor3})
       : super(key: key);
 
   @override
@@ -78,11 +78,11 @@ class CreateCardsState extends State<CreateCards> {
   int passColor1, passColor2, passColor3;
   CreateCardsState(
       {Key key,
-      @required this.passData,
-      @required this.passDataName,
-      @required this.passColor1,
-      @required this.passColor2,
-      @required this.passColor3});
+        @required this.passData,
+        @required this.passDataName,
+        @required this.passColor1,
+        @required this.passColor2,
+        @required this.passColor3});
 
   @override
   Widget build(BuildContext context) {
@@ -105,7 +105,7 @@ class CreateCardsState extends State<CreateCards> {
   Widget buildBody(BuildContext context, List<DocumentSnapshot> snapshot) {
     return ListView(
       children:
-          snapshot.map<Widget>((data) => buildListItem(context, data)).toList(),
+      snapshot.map<Widget>((data) => buildListItem(context, data)).toList(),
     );
   }
 
@@ -117,138 +117,144 @@ class CreateCardsState extends State<CreateCards> {
 
     List<String> nutritionList = ['Kalori', 'Karbonhidrat', 'Yağ', 'Protein'];
 
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 0.0),
-          child: Row(
-            children: [
-              Stack(
-                alignment: Alignment.topLeft,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(left: leftPadding),
-                    child: Container(
-                      height: heightH - leftPadding,
-                      width: widthH - leftPadding,
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        'Besin Degerleri',
-                        style: TextStyle(
-                            fontSize: 25.0,
-                            color: Color.fromRGBO(
-                                passColor1, passColor2, passColor3, 1),
-                            fontFamily: 'Berlin Sans FB Regular'),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 0.0),
+            child: Row(
+              children: [
+                Stack(
+                  alignment: Alignment.topLeft,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left: leftPadding),
+                      child: Container(
+                        height: heightH - leftPadding,
+                        width: widthH - leftPadding,
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Besin Degerleri',
+                          style: TextStyle(
+                              fontSize: 25.0,
+                              color: Color.fromRGBO(
+                                  passColor1, passColor2, passColor3, 1),
+                              fontFamily: 'Berlin Sans FB Regular'),
+                        ),
                       ),
                     ),
-                  ),
-                  Positioned(
-                    right: 15,
-                    top: 20,
-                    child: Container(
-                        width: widthH / 2,
-                        height: widthH / 2,
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: NetworkImage(row.photo),
-                                fit: BoxFit.fill))),
-                  ),
-                  Positioned(
-                    top: 50,
-                    left: 10,
-                    child: Container(
-                      width: 125,
-                      height: heightH,
-                      child: ListView.builder(
-                          itemCount: nutritionList.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Padding(
-                              padding: const EdgeInsets.all(4.0),
-                              child: Container(
-                                height: heightH / 6,
-                                child: Text(
-                                  nutritionList[index],
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 20.0),
-                                ),
-                              ),
-                            );
-                          }),
+                    Positioned(
+                      right: 15,
+                      top: 20,
+                      child: Container(
+                          width: widthH / 2,
+                          height: widthH / 2,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: NetworkImage(row.photo),
+                                  fit: BoxFit.fill))),
                     ),
-                  ),
-                  Positioned(
-                    top: 40,
-                    left: widthH / 3.2,
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: 55,
-                      height: heightH, 
-                      child: ListView.builder(
-                          itemCount: row.nutritions.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Padding(
-                              padding: const EdgeInsets.all(4.0),
-                              child: Container(
-                                height: heightH / 6,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5.0),
-                                    color: Color.fromRGBO(
-                                        passColor1, passColor2, passColor3, 1)),
-                                child: Center(
+                    Positioned(
+                      top: 50,
+                      left: 10,
+                      child: Container(
+                        width: 125,
+                        height: heightH,
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: nutritionList.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Container(
+                                  height: heightH / 6,
                                   child: Text(
-                                    row.nutritions[index],
+                                    nutritionList[index],
                                     style: TextStyle(
-                                        color: Colors.white, fontSize: 20.0),
+                                        color: Colors.black, fontSize: 20.0),
                                   ),
                                 ),
-                              ),
-                            );
-                          }),
+                              );
+                            }),
+                      ),
                     ),
-                  ),
-                ],
+                    Positioned(
+                      top: 40,
+                      left: widthH / 3.2,
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 55,
+                        height: heightH,
+                        child: ListView.builder(
+                            itemCount: row.nutritions.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Container(
+                                  height: heightH / 6,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5.0),
+                                      color: Color.fromRGBO(
+                                          passColor1, passColor2, passColor3, 1)),
+                                  child: Center(
+                                    child: Text(
+                                      row.nutritions[index],
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20.0),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 25.0,
+          ),
+          Column(
+            children: [
+              Text(
+                'Malzemeler',
+                style: TextStyle(
+                    fontSize: 28.0,
+                    color: Color.fromRGBO(passColor1, passColor2, passColor3, 1),
+                    fontFamily: 'Berlin Sans FB Regular'),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 14),
+                child: Container(
+
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: row.ingredients.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(1.3),
+                          child: new Text(
+                            '• ' + row.ingredients[index],
+                            style: TextStyle(fontSize: 28),
+                          ),
+                        );
+                      }),
+                ),
+              ),
+              Text(
+                'Yapılısı',
+                style: TextStyle(
+                    fontSize: 28.0,
+                    color: Color.fromRGBO(passColor1, passColor2, passColor3, 1),
+                    fontFamily: 'Berlin Sans FB Regular'),
               ),
             ],
           ),
-        ),
-        SizedBox(
-          height: 25.0,
-        ),
-        Column(
-          children: [
-            Text(
-              'Malzemeler',
-              style: TextStyle(
-                  fontSize: 28.0,
-                  color: Color.fromRGBO(passColor1, passColor2, passColor3, 1),
-                  fontFamily: 'Berlin Sans FB Regular'),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Container(
-              alignment: Alignment.center,
-              width: widthH / 1.1,
-              height: heightH,
-              child: ListView.builder(
-                  itemCount: row.ingredients.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return new Text(
-                      '• ' + row.ingredients[index],
-                      style: TextStyle(fontSize: 18),
-                    );
-                  }),
-            ),
-            Text(
-              'Yapılısı',
-              style: TextStyle(
-                  fontSize: 28.0,
-                  color: Color.fromRGBO(passColor1, passColor2, passColor3, 1),
-                  fontFamily: 'Berlin Sans FB Regular'),
-            ),
-          ],
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
